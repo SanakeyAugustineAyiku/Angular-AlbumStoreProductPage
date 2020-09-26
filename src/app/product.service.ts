3e6af7c22f695fada931a4a55ab41ecb8ab12999
Augustine Sanakey <sanakeyaugustineayiku@gmail.com>
2020-09-26 21:34:43 +0000

    changed fixed variable name missmatch

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import { Product } from "./product";
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
-  private _productUrl: string = "../assets/products.json";
+  private _productsUrl: string = "../assets/products.json";
   
   constructor(private _http: Http) { }
   
@@ -15,8 +15,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getProduct():Observable<Product[]>{
-    return this._http.get(this._productUrl).map(response=> <Product[]>response.json());
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response=> <Product[]>response.json());
   }
 
 }
